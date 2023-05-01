6da1ace597773eabdf8a32eeee28497c8d164559
gisellepbn <giselle.penton@gmail.com>
2023-05-01 12:07:49 -0400

    Create models Profile and Plans

--- healthtracker/settings.py
+++ healthtracker/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'plans.Profile'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Profile(AbstractUser):
+
+    name = models.CharField(max_length=100)
+    profile_picture = models.ImageField(null=True, upload_to='profile/')
+
+    class Meta:
+        ordering = ['name']
+
+    
+    def __str__(self):
+        return self.username
+
+
+class Plan:
+
+    CATEGORIES = [
+         ('Nutrition', 'Nutrition'),
+         ('Water_Intake', 'Water Intake'),
+         ('Exercise', 'Exercise'),
+         ('Sleep', 'Sleep')
+    ]
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateField()
+    category = models.CharField(max_length=100, choices=CATEGORIES)
+    parameter = models.CharField(max_length=200)
+    goal = models.FloatField()
+    progress = models.FloatField()
+    unit = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add= True, editable=False)
+
+    class Meta:
+        ordering = ['-date', 'category']
+
+    def __str__(self):
+        return self.parameter
