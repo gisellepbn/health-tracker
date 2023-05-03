453f997c078ed9b80c29aff050cf8d9a20ac4a20
gisellepbn <giselle.penton@gmail.com>
2023-05-03 17:21:58 -0400

    Delete old profile image file when updating the profile

--- plans/signals.py
+++ plans/signals.py
@@ -1,7 +1,20 @@
-from django.db.models.signals import post_save, post_delete, pre_delete
+from django.db.models.signals import pre_save, post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from .models import Profile
 import os
+
+
+@receiver(pre_save, sender=Profile)
+def delete_old_profile_picture_on_update(sender, instance, **kwargs):
+    if instance.id:
+            try:
+                old_picture = Profile.objects.get(id=instance.id).picture                
+            except Profile.DoesNotExist:
+                return
+            else:
+                new_picture = instance.picture
+                if old_picture and old_picture.url != new_picture.url:
+                    old_picture.delete(save=False)
 
 
 @receiver(pre_delete, sender=Profile)
